fe22f12fe4646117d8719d0db23f001a6cdb2858
Ilya Gutnikov <IlyaGutnikov@lab1n00.iu3.bmstu.ru>
2015-11-11 13:30:32 +0400

    Сделана первая часть лабы. 1 с = 50 000 000 тактов

---
+++ niosII_pwm_project/obj/default/pwm.d
@@ -0,0 +1,14 @@
+obj/default/pwm.o: pwm.c altera_avalon_pwm_regs.h \
+ ../niosII_pwm_project_bsp//HAL/inc/io.h \
+ ../niosII_pwm_project_bsp//HAL/inc/alt_types.h \
+ ../niosII_pwm_project_bsp/system.h ../niosII_pwm_project_bsp/linker.h
+
+altera_avalon_pwm_regs.h:
+
+../niosII_pwm_project_bsp//HAL/inc/io.h:
+
+../niosII_pwm_project_bsp//HAL/inc/alt_types.h:
+
+../niosII_pwm_project_bsp/system.h:
+
+../niosII_pwm_project_bsp/linker.h:
