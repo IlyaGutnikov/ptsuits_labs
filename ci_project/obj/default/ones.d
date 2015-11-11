5bfc1931b9f14812fb6e624dec5c069f80e02f88
Ilya Gutnikov <IlyaGutnikov@lab1n00.iu3.bmstu.ru>
2015-11-11 14:05:21 +0400

    Втораячасть лабораторной работы

---
+++ ci_project/obj/default/ones.d
@@ -0,0 +1,18 @@
+obj/default/ones.o: ones.c \
+ ../niosII_pwm_project_bsp//drivers/inc/altera_avalon_performance_counter.h \
+ ../niosII_pwm_project_bsp//HAL/inc/alt_types.h \
+ ../niosII_pwm_project_bsp//HAL/inc/io.h \
+ ../niosII_pwm_project_bsp//HAL/inc/alt_types.h \
+ ../niosII_pwm_project_bsp/system.h ../niosII_pwm_project_bsp/linker.h
+
+../niosII_pwm_project_bsp//drivers/inc/altera_avalon_performance_counter.h:
+
+../niosII_pwm_project_bsp//HAL/inc/alt_types.h:
+
+../niosII_pwm_project_bsp//HAL/inc/io.h:
+
+../niosII_pwm_project_bsp//HAL/inc/alt_types.h:
+
+../niosII_pwm_project_bsp/system.h:
+
+../niosII_pwm_project_bsp/linker.h:
